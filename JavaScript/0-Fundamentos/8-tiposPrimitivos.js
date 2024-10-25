5c1c15450b2b41f39e4df107b1236e707202ba60
Higor Moreira <igore.m.oliveira@gmail.com>
2024-10-25 18:33:59 -0300

    Create 8-tiposPrimitivos.js

---
+++ JavaScript/0-Fundamentos/8-tiposPrimitivos.js
@@ -0,0 +1,17 @@
+/*
+    Para saber o tipo de um dado usa-se o typeof + nome da variavel ou dado literal. 
+
+    1 - Number
+    2 - String
+    3 - bolean
+    4 - Null
+*/
+
+var numero = 10
+var nome = 'Igor'
+var vivo = true
+
+console.log(typeof numero)
+console.log(typeof nome)
+console.log(typeof vivo)
+
