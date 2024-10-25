b9294719f833ecf49d55fa1cf2aeae3acb0aa0a4
Higor Moreira <igore.m.oliveira@gmail.com>
2024-10-25 18:34:03 -0300

    Create 6-nomeacaoVariavel.js

---
+++ JavaScript/0-Fundamentos/6-nomeacaoVariavel.js
@@ -0,0 +1,20 @@
+// Regras para nomeação de variavel
+
+// 1° Primeiro caractere deve ser letra, uderline ou cifrão
+// 2° Após o primeiro, pode ser adicinado numeros de 0 ~ 9
+// 3° case-sensitive maiuscula e minuscula são diferentes. 
+// 4° Apesar de aceito, não é recomendado o uso de acentuação. 
+
+/*
+
+Convenção do JS é o CamelCase, Ou seja, Primeira letraa da primeira palavra
+variavel é minuscula, e caso tenha mais palavras, a primeira letra será maiuscula. 
+
+*/
+
+var anoNascimento = 1996
+
+var segundoNumero = 2
+
+var nomeCompleto = 'Igor Oliveira'
+
