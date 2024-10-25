5803f99ad2bf4f7303e8ea1e5a0111785df9f31d
Higor Moreira <igore.m.oliveira@gmail.com>
2024-10-25 18:33:45 -0300

    Delete 6-nomeacaoVariavel.js

--- JavaScript/Fundamentos/6-nomeacaoVariavel.js
+++
@@ -1,7 +0,0 @@
-// Regras para nomeação de variavel
-
-// 1° Primeiro caractere deve ser letra, uderline ou cifrão
-// 2° Após o primeiro, pode ser adicinado numeros de 0 ~ 9
-// 3° case-sensitive maiuscula e minuscula são diferentes. 
-// 4° Apesar de aceito, não é recomendado o uso de acentuação. 
-
