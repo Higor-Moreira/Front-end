4bed4b70765f085aefcdd7ebb0c9f246c1e378b7
Higor Moreira <igore.m.oliveira@gmail.com>
2024-10-25 09:00:23 -0300

    Create 3-desafio.js

---
+++ JavaScript/Fundamentos/3-desafio.js
@@ -0,0 +1,21 @@
+// Desafio Exibir numeros impares e pares em blocos
+
+{
+    console.log(">>> NUMEROS IMPARES <<<")
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)    
+}
+
+
+{
+    console.log(">>> NUMEROS PARES <<<")
+    console.log(2)
+    console.log(4)
+    console.log(6)    
+    console.log(8)    
+    console.log(10)    
+}
+
