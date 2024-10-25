86edd7fab5e7c88b4f69be138ef9c9534416ceb1
Higor Moreira <igore.m.oliveira@gmail.com>
2024-10-25 18:34:01 -0300

    Create 7-trocarValores.js

---
+++ JavaScript/0-Fundamentos/7-trocarValores.js
@@ -0,0 +1,15 @@
+// trocar o valor de 2 variaveis, onde ao fim, uma terá o valor da outra 
+
+let a = 7
+let b = 94
+
+console.log('a =', a)
+console.log('b =', b)
+
+let c = a
+a = b
+b = c
+
+console.log('a =', a)
+console.log('b =', b)
+
