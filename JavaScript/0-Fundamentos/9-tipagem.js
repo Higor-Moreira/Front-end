85e67645b9e51c729f181865c4bcb06bae3d493d
Higor Moreira <igore.m.oliveira@gmail.com>
2024-10-26 16:41:51 -0300

    Create 9-tipagem.js

---
+++ JavaScript/0-Fundamentos/9-tipagem.js
@@ -0,0 +1,21 @@
+// JS é uma linguagem de tipagem dinâmica, isto é, tipagem fraca.  
+
+/*
+
+    Uma linguagem com tipagem fraca é aquela em que o sistema de 
+    tipos permite mais flexibilidade na forma como os tipos de dados 
+    são utilizados. Isso significa que a linguagem pode realizar 
+    conversões automáticas entre tipos diferente.
+
+
+*/
+
+console.log('5' / 2) // Somar uma string a um number 
+
+/*
+
+    Em uma linguagem de tipagem forte, a operação acima iria gerar um erro.
+    Por exemplo em Python, não é ocorre a conversão automatica.
+
+*/
+
