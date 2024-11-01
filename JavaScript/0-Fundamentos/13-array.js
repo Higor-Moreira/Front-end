4ebb863300871387866b008fe5101aacc94ac71f
Higor Moreira <igore.m.oliveira@gmail.com>
2024-11-01 19:05:50 -0300

    Update 13-array.js

--- JavaScript/0-Fundamentos/13-array.js
+++ JavaScript/0-Fundamentos/13-array.js
@@ -1,18 +1,19 @@
 /*
-     Estrutura de dados array ou lista, dados agrupados de forma linear, 
-     onde os elementos são indexados e casa um tem seu identificador. 
+     A estrutura array consiste em dados agrupados de forma linear, 
+     onde os elementos são indexados e cada um tem seu identificador. 
 
-     1 - o elemento esta na posição zero 
-     2 - não tem tamanho fixo, pode aumentar conforme necessidade
+     1 - Inicia em zero 
+     2 - Não tem tamanho fixo, pode aumentar conforme necessidade
      3 - É definido por colchetes 
-     4 - dentro do array, os valores são separados por virgula. 
-     5 - Pode misturar os tipos de dados. Porem, não é recomendado. 
+     4 - Os valores armazenados são separados por virgula. 
+     5 - É possivel misturar os tipos de dados. Porem, não é recomendado. 
+     6 - Em JS o array é do tipo Object. 
 
 */
 
 const valores = [7, 8.5, 10, 9.7] // array possui 4 elementos 
 
-// Como consultar por indicce
+// Como consultar por indice
 console.log(valores[0], valores[2])
 
 // O que acontece se consultar um indice inexistente? 
