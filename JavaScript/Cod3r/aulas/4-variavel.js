72f8eff24fbb4d7e7f8cc263e9a2e1289151ed38
Higor Moreira <igore.m.oliveira@gmail.com>
2024-11-18 15:56:01 -0300

    Create 4-variavel.js

---
+++ JavaScript/Cod3r/aulas/4-variavel.js
@@ -0,0 +1,37 @@
+// Primeira forma de declarar variavel
+
+nome = 'Caneta'
+quantidade = 10
+preco = 6.4
+imposto = 1.5
+precoFinal = preco + imposto
+
+
+
+// 2° Variavel escopo global - VAR
+
+var nome = 'Caneta'
+var quantidade = 10
+var preco = 6.4
+var imposto = 1.5
+var precoFinal = preco + imposto
+
+
+
+// 3° Variavel escopo local - LET
+
+let nome = 'Caneta'
+let quantidade = 10
+let preco = 6.4
+let imposto = 1.5
+let precoFinal = preco + imposto
+
+
+
+// 4º 'Variavel' constante - const
+
+const nome = 'Igor'
+const anoNascimento = 1996
+const pi = 3.14
+
+
