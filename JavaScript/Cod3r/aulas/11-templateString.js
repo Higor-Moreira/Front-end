dad1ea911a7a583aae612ca1c54bbfa52baca654
Higor Moreira <igore.m.oliveira@gmail.com>
2024-11-18 15:54:58 -0300

    Create 11-templateString.js

---
+++ JavaScript/Cod3r/aulas/11-templateString.js
@@ -0,0 +1,18 @@
+/*
+    Template String é uma forma de trabalhar com strings e variavies.
+*/
+
+// Forma padrão 
+let nome = 'Higor'
+let mensagem = 'Olá ' + nome
+console.log(mensagem)
+
+
+
+// Template String 
+let outraMensagem = `Olá novamente, ${nome}
+É bom te ver por aqui outra vez.`
+console.log(outraMensagem)
+
+// Template String usa crase, não aspas
+
